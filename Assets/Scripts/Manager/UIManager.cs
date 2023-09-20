b3a064a4efb7e0a62d44d78b2dea506d7aa741f1
JangbeomChoi <jangbeom0325@gmail.com>
2023-09-20 15:03:35 +0900

    [Add] 뒤로가기 버튼, 이미지, 스크립트 추가

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject statusUIPanel;
+    public Button statusButton;
+    public Button inventoryButton;
+    
+    void Start()
+    {
+        statusButton.onClick.AddListener(ShowStatusUI);
+    }
+
+    public void ShowStatusUI()
+    {
+        statusUIPanel.SetActive(true);
+
+        statusButton.gameObject.SetActive(false);
+        inventoryButton.gameObject.SetActive(false);
+
+    }
+
+    public void CloseStatusUI()
+    {
+        statusUIPanel.SetActive(false);
+
+        statusButton.gameObject.SetActive(!false);
+        inventoryButton.gameObject.SetActive(!false);
+    }
+}
