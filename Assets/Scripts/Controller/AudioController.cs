3d1058fbc5bb5e9810eb7aa28f4ee9073478263a
JangbeomChoi <jangbeom0325@gmail.com>
2023-09-21 14:59:59 +0900

    [Add/Fix] 음소거 토글 추가 및 버그 목록 2,3번 픽스
    -메인화면과 시작화면 볼륨 슬라이더 동기화 완료
    -설정창에서 게임시작화면으로 돌아간뒤 볼륨 리셋 안되던 문제 해결

--- Assets/Scripts/Controller/AudioController.cs
+++ Assets/Scripts/Controller/AudioController.cs
@@ -5,17 +5,72 @@ public class AudioController : MonoBehaviour
 {
     public AudioMixer masterMixer;
     public Slider audioSlider;
+    public Toggle audioToggle; // 음소거 토글 추가
+
+    private const string BGMVolumeKey = "BGMVolume";
+    private const string AudioMutedKey = "AudioMuted";
+
+    private void Start()
+    {
+        // PlayerPrefs에서 저장된 설정 값을 읽어와서 UI에 반영
+        float savedBGMVolume = PlayerPrefs.GetFloat(BGMVolumeKey, 0f);
+        bool audioMuted = PlayerPrefs.GetInt(AudioMutedKey, 0) == 1;
+
+        audioSlider.value = savedBGMVolume;
+        audioToggle.isOn = audioMuted;
+
+        // 초기 설정을 적용
+        SetBGMVolume(savedBGMVolume);
+        SetAudioMuted(audioMuted);
+    }
 
     public void AudioControl()
     {
         float sound = audioSlider.value;
 
-        if (sound == -40f) masterMixer.SetFloat("BGM", -80);
-        else masterMixer.SetFloat("BGM", sound);
+        // 설정 값을 PlayerPrefs에 저장
+        PlayerPrefs.SetFloat(BGMVolumeKey, sound);
+        PlayerPrefs.Save();
+
+        SetBGMVolume(sound);
     }
 
     public void ToggleAudioVolume()
     {
-        AudioListener.volume = AudioListener.volume == 0 ? 1 : 0;
+        bool audioMuted = audioToggle.isOn;
+
+        // 설정 값을 PlayerPrefs에 저장
+        PlayerPrefs.SetInt(AudioMutedKey, audioMuted ? 1 : 0);
+        PlayerPrefs.Save();
+
+        SetAudioMuted(audioMuted);
+    }
+
+    private void SetBGMVolume(float volume)
+    {
+        if (volume == -40f) masterMixer.SetFloat("BGM", -80);
+        else masterMixer.SetFloat("BGM", volume);
+    }
+
+    private void SetAudioMuted(bool muted)
+    {
+        AudioListener.volume = muted ? 0 : 1;
     }
 }
+//{
+//    public AudioMixer masterMixer;
+//    public Slider audioSlider;
+
+//    public void AudioControl()
+//    {
+//        float sound = audioSlider.value;
+
+//        if (sound == -40f) masterMixer.SetFloat("BGM", -80);
+//        else masterMixer.SetFloat("BGM", sound);
+//    }
+
+//    public void ToggleAudioVolume()
+//    {
+//        AudioListener.volume = AudioListener.volume == 0 ? 1 : 0;
+//    }
+//}
